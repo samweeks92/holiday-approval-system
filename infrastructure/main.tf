47dbacc68efaa57c4a5f0d8634f6639285b0bfaf
Sam Weeks <weekss@google.com>
2026-08-10 15:42:54 +0100

    second commit

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,17 +1,3 @@
-# module "backend" {
-
-#   # Set Source
-#   source = "./modules/backend"
-
-#   # Define Environment Variables
-#   github-host-connection-name = var.github-host-connection-name
-#   github-repo-name            = var.github-repo-name
-#   project                     = var.project
-#   region                      = var.region
-#   demo-name                   = var.demo-name
-
-# }
-
 module "frontend" {
 
   # Set Source
